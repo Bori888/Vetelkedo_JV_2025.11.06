9dff23b0b0d47d6c4424754c44d13123d155a4b1
Bori888 <ilyes.bori8@gmail.com>
2025-11-06 13:31:25 +0100

    PlyerModell kesz

---
+++ src/modell/PlayerModell.java
@@ -0,0 +1,32 @@
+package modell;
+
+public class PlayerModell {
+    private int valasztottAjto;//-1 ha nincs
+    private boolean valtott;//true ha változtatott
+
+    public PlayerModell() {
+        this.valasztottAjto = -1;
+        this.valtott = false;
+    }
+
+    public int getValasztottAjto() {
+        return valasztottAjto;
+    }
+
+    public boolean isValtott() {
+        return valtott;
+    }
+
+    public void setValasztottAjto(int valasztottAjto) {
+        this.valasztottAjto = valasztottAjto;
+    }
+
+    public void setValtott(boolean valtott) {
+        this.valtott = valtott;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerModell{" + "valasztottAjto=" + valasztottAjto + ", valtott=" + valtott + '}';
+    }
+}
